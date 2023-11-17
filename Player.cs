ed06559b90ce8572ee4947b9bce3e29b598d006a
WilliamKSilva <wilz.kelvin12@gmail.com>
2023-11-17 18:28:15 -0300

    feat(player): add basic Player 2d movement, move forward, backwards, jump, gravity, speed

--- Player.cs
+++ Player.cs
@@ -3,15 +3,48 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
+	[Export]
+	public int Speed { get; set; } = 200;
 
-	public const float Gravity = 2000.0f;
+	public int JumpSpeed = -400;
+	public int Gravity = 200;
 
 	public override void _PhysicsProcess(double delta)
 	{
+		Vector2 velocity = Vector2.Zero;
+
+		if (Input.IsActionPressed("move_right"))
+		{
+			velocity.X += 1;
+		}
+
+		if (Input.IsActionPressed("move_left"))
+		{
+			velocity.X -= 1;
+		}
+
+		Timer timer = GetNode<Timer>("JumpTimer");
+		GD.Print(timer.TimeLeft);
+
+		if (Input.IsActionJustPressed("jump") && timer.TimeLeft == 0)
+		{
+			GD.Print("Pressed");
+			timer.Start();
+		}
+
+		if (Input.IsActionPressed("jump") && timer.TimeLeft > 0)
+		{
+			velocity.Y = JumpSpeed;
+		}
+
 		if (!IsOnFloor())
 		{
-			MoveAndCollide(new Vector2(0, 15));	
+			velocity.Y += Gravity;
 		}
+
+		velocity.X *= Speed;
+		Velocity = velocity;
+
+		MoveAndSlide();
 	}
 }
