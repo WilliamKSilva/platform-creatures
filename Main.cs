b45bd0a8bd20ba3e9b5c51f838276c8cdfaecbf1
WilliamKSilva <wilz.kelvin12@gmail.com>
2023-11-20 21:02:24 -0300

    feat: add logic to Main node to deal with Level instatiation and NPC position set

--- Main.cs
+++ Main.cs
@@ -8,15 +8,33 @@ public partial class Main : Node
 
 	public override void _Ready()
 	{
-		JsonHelper.Parse("grawg");
+		// JsonHelper.Parse("grawg");
 
+		// Connect signal to detect player / NPC collision
 		var signals = GetNode<Signals>("/root/Signals");
 		signals.OnPlayerCollision += PlayerCollision;
+
+		// Render current level
+		InstantiateLevel(LevelScene);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	public void InstantiateLevel(PackedScene scene)
+	{
+		var sceneInstantiaded = scene.Instantiate();
+		SetNPCPosition("Grawg", sceneInstantiaded);
+		AddChild(sceneInstantiaded);
+	}
+
+	public void SetNPCPosition(string npcName, Node scene)
+	{
+		var npc = scene.GetNode<Node2D>($"{npcName}");
+		Marker2D positionNode = npc.GetNode<Marker2D>("Position");
+		npc.Position = new Vector2(positionNode.Position.X, positionNode.Position.Y);
 	}
 
 	public void ShowNPCDialog(string dialogText, NPC npc)
