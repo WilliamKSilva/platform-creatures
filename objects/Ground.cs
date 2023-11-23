b3e3f55731eea35f1c4a655b27592e9f39c74d78
WilliamKSilva <wilz.kelvin12@gmail.com>
2023-11-22 23:33:41 -0300

    fix: remove dynamic polygon variable size of Ground and Roof

--- objects/Ground.cs
+++ objects/Ground.cs
@@ -3,18 +3,10 @@ using System;
 
 public partial class Ground : StaticBody2D
 {
-  [Export]
-  public int PolygonXWidth { get; set; } 
-
   public override void _Ready()
   {
     Polygon2D polygon = GetNode<Polygon2D>("Polygon2D");
     var polygonShape = polygon.Polygon;
-
-    polygonShape[2].X = PolygonXWidth;
-    polygonShape[3].X = PolygonXWidth;
-    polygonShape[4].X = PolygonXWidth;
-
     polygon.Polygon = polygonShape;
 
     CollisionPolygon2D polygonCollision = GetNode<CollisionPolygon2D>("CollisionPolygon2D");
--- objects/Roof.cs
+++ objects/Roof.cs
@@ -3,20 +3,10 @@ using System;
 
 public partial class Roof : StaticBody2D
 {
-  [Export]
-  public int PolygonXWidth { get; set; }
-
   public override void _Ready()
   {
     Polygon2D polygon2D = GetNode<Polygon2D>("Polygon2D");
     var polygonShape = polygon2D.Polygon;
-
-    GD.Print(polygon2D.Polygon[2]);
-
-    polygonShape[26].X = PolygonXWidth;
-    polygonShape[27].X = PolygonXWidth + 300;
-    polygonShape[28].X = PolygonXWidth + 500;
-
     polygon2D.Polygon = polygonShape;
 
     CollisionPolygon2D polygon2DCollision = GetNode<CollisionPolygon2D>("CollisionPolygon2D");
